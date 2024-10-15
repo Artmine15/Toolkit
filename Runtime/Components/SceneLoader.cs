933b96860378166f341d688e285f1677510a8db0
Artem <artsel256@gmail.com>
2024-10-15 20:57:41 +0700

    (1.0.9) Added safe scene loading

--- Runtime/Components/SceneLoader.cs
+++ Runtime/Components/SceneLoader.cs
@@ -2,16 +2,33 @@ using UnityEngine.SceneManagement;
 
 namespace Artmine15.Utils.Toolkit.Components
 {
-    public class SceneLoader
+    public static class SceneLoader
     {
+        private static bool _isSceneLoading;
+
         public static void LoadScene(int buildIndex)
         {
+            if (_isSceneLoading == true) return;
+            else DisableLoading();
+
             SceneManager.LoadScene(buildIndex);
         }
 
         public static void ReloadScene()
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        private static void EnableLoading(Scene scene, LoadSceneMode loadSceneMode)
+        {
+            _isSceneLoading = false;
+            SceneManager.sceneLoaded -= EnableLoading;
+        }
+
+        private static void DisableLoading()
+        {
+            _isSceneLoading = true;
+            SceneManager.sceneLoaded += EnableLoading;
         }
     }
 }
